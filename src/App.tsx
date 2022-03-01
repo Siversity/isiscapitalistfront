230b97ef97bc97641f8905fbbe8e0e65fda35c4d
Stéphane <81stephane@gmail.com>
2022-03-01 16:35:15 +0100

    Initialisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { Services } from "./Services";
+import { World, Product, Pallier } from './world';
+
+console.log("Je démarre initial")
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log("Je démarre")
+
+  let world = GetData();
+
+
+
+  return showHTML(world);
 }
 
+// Obtention données
+function GetData() {
+  const [services, setServices] = useState(new Services(""))
+  const [world, setWorld] = useState(new World())
+  const username = "TestWorld";
+
+  useEffect(() => {
+    let services = new Services(username)
+    setServices(services)
+    services.getWorld().then(response => {
+      console.log("J'affiche les data")
+      console.log(response.data)
+      setWorld(response.data)
+    }
+    )
+  }, [])
+
+  return world;
+}
+
+function test(world : World) {
+  return (<div>{world.lastupdate}</div>);
+}
+
+function showHTML(world : World) {
+  // HTML array output
+  let buffer = []
+
+  // HTML test
+  let testDisplay = test(world);
+  let testDisplay2 = test(world);
+
+  buffer.push(testDisplay);
+  buffer.push(testDisplay2);
+
+  let output = buffer;
+
+  // Affichage client
+  return (<div>{buffer}</div>);
+}
+
+
+
+
 export default App;
+
